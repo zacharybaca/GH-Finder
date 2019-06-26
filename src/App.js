31d3095b98f320ec13155a4979e783b292e2c2ee
cubsfan46350 <cubsfan.46350@msn.com>
2019-06-26 09:20:16 -0500

    fixed syntax error, added user item

--- src/App.js
+++ src/App.js
@@ -7,13 +7,15 @@ import Users from './components/users/Users';
 import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
 import About from './components/pages/About';
+import User from './components/users/User';
 
 class App extends Component {
   state = {
     users: [],
+    user: {},
     loading: false,
     alert: null
-  }
+  };
   
 
   searchUsers = async (text) => {
@@ -24,6 +26,14 @@ class App extends Component {
     this.setState({ users: res.data.items, loading: false });
   };
 
+  getUser = async (username) => {
+    this.setState({ loading: true });
+    const res = await axios.get(`https://api.github.com/users/${username}?client_id=$
+    {process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=$
+    {process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    this.setState({ user: res.data, loading: false });
+  };
+
   clearUsers = () => {
     this.setState({ users: [], loading: false });
   }
@@ -32,6 +42,9 @@ class App extends Component {
     this.setState({ alert: { msg: msg, type: type }});
     setTimeout(() => this.setState({ alert: null }), 5000);
   }
+  
+
+  
   render() {
   return (
     <Router>
@@ -47,13 +60,19 @@ class App extends Component {
           </Fragment>
         )}/>
         <Route exact path='/about' component={About} />
+        <Route exact path='/user/:login' render={(props) => (
+          <User {...props} getUser={this.getUser} user={this.state.user} loading={this.state.loading} />
+        )}/>
       </Switch>
       
       </div>
     </div>
     </Router>
   );
+  }
 }
-}
+
+
+
 
 export default App;
